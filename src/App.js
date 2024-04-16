6bc4aa5dbfda8a8f254373af83b33ca8d8b518e3
Homiiiiiiiiiiiiiiiiii <entekk1112@naver.com>
2024-04-16 17:36:55 +0900

    event복습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+    const handClick = (e) => {
+      alert('click')
+    }
+
+    const handleMouseOver = (e) => {
+      e.target.style.backgroundColor = "skyblue"
+    }
+
+    const handleMouseOut = (e) => {
+      e.target.style.backgroundColor = 'white'
+    }
+
+    const handleChange = (e) => {
+      document.querySelector(".text-value").innerText = e.target.value
+    }
+
+    const [count, setCount] = useState(0);
+    const handleButtonUp = (e) => {
+      setCount(count + 1)
+    }
+
+    const handleButtonDown = (e) => {
+      setCount(count - 1)
+    }
+
+    const [inputValue, setinputValue] = useState("")
+    const handelInputChange = (e) => {
+      setinputValue(e.target.value)
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>입력 이벤트 예제</h1>
+      <input type="text" onChange={handelInputChange} value={inputValue} placeholder="문자를 입력해주세요."/>
+      <p>입력한 값 : {inputValue}</p>
+
+      <h1>클릭 이벤트 예제</h1>
+      <button onClick={handleButtonUp}>+</button>
+      <button onClick={handleButtonDown}>-</button>
+      <div>{count}</div>
+      {count >= 5 ? <p>5 이상입니다.</p> :<p>5 이하입니다.</p>}
+
+
+      <h1>Click Event</h1>
+      <button onClick={handClick}>button</button>
+
+      <h1>Mouse Over Event</h1>
+      <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>마우스를 올려주세요.</div>
+
+      <h1>Input Event</h1>
+      <input type="text" onChange={handleChange} placeholder="문자를 입력해주세요."/>
+      <div className="text-value"></div>
     </div>
   );
 }
